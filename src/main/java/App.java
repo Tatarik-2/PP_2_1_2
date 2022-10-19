a4c7398eeeca7c628be196dec4c2e5cfa3df6c3c
Tatarik-2 <Tatarin161616@yandex.ru>
2022-10-19 21:33:51 +0300

    mark 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("catBean");
+        Cat catBean2 = (Cat) applicationContext.getBean("catBean");
+        System.out.println(bean==bean2);
+        System.out.println(catBean==catBean2);
     }
 }
